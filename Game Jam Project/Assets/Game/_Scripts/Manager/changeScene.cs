dff6679b5b9ef5be41f8f2ab8a764485a455aa9e
Oliver <ojaypopedev@gmail.com>
2018-11-30 18:25:07 +0000

    Added Prefabs
    +Added Level Core Prefab that can be applied to any build levels. It contains a canvas with "reset" & "menu" buttons., as well as a Level Manager, which contains the Script "Change Scene".
    +Added Start Scene
    +Added Credits Scene

---
+++ Game Jam Project/Assets/Game/_Scripts/Manager/changeScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+
+    int thisScene;
+
+    private void Start()
+    {
+        thisScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void GoToScene(int Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(thisScene);
+    }
+
+   
+}
